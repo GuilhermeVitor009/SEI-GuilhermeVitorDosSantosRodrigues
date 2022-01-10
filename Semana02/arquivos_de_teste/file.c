08bdf89f5bbfc34d6b4f172a663694aa4d1cd8d5
GuilhermeVitor009 <81776213+GuilhermeVitor009@users.noreply.github.com>
2022-01-10 16:32:32 -0300

    Add files via upload

---
+++ Semana02/arquivos_de_teste/file.c
@@ -0,0 +1,12 @@
+
+  /*
+   * Listing 3.1
+   * hello.c – Canonical “Hello, world!” program 4   4  */
+  #include <stdio.h>
+
+ int main(void)
+ {
+     fprintf(stdout, "Hello, Linux programming world!\n");
+      return 0;
+  }
+
