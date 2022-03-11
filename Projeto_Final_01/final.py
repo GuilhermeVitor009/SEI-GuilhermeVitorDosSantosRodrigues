c9c1f2d75cabc6ee8aab724b6f60fdbe704437e7
GuilhermeVitor009 <GuilhermeVitor009@github.com>
2022-03-11 15:07:39 -0300

    Semana09

--- Projeto_Final_01/final.py
+++ Projeto_Final_01/final.py
@@ -9,7 +9,7 @@ from PID import *
 import sys
 
 
-#Variaveis iniciais
+# Variaveis iniciais
 step = 1
 w1 = 0
 w2 = 0
@@ -17,10 +17,10 @@ v1 = 0
 v2 = 0
 mx, my = 0, 0
 vel_ang = 0
-pos = [400, 500]
+pos = [390, 420]
 angulo = 0
-eP = np.array([1,1]) #Erro da posição
-ePhi = 2  #Erro do angulo
+eP = np.array([1, 1])  # Erro da posição
+ePhi = 2  # Erro do angulo
 estavel = False
 auto_move = False
 y_direction = 0
@@ -29,26 +29,27 @@ Fy = 0
 F = 0
 vel_vert = 0
 vel_horiz = 0
-x = np.array([w1, w2, pos[0], pos[1], v1, v2, angulo * np.pi/180., vel_ang*np.pi/180.])
-#Dados do drone
+x = np.array([w1, w2, pos[0], pos[1], v1, v2,
+             angulo * np.pi/180., vel_ang*np.pi/180.])
+# Dados do drone
 m = 0.25
 g = 9.79
 P = m*g
 
 
 def acelerar(t, F, estavel):
-    comando = pygame.key.get_pressed()
+    print(estavel)
     if estavel == False:
-        #print(F)
+        # print(F)
         F += 100*t
         if F >= 155:
             F = 155
     else:
-         print(F)
-         F -= 100*t
-         #print(t)
-         if F <= 0:
-             F = 0
+        # print(F)
+        F -= 100*t
+        # print(t)
+        if F <= 0:
+            F = 0
     return F
     # return F
 
@@ -56,31 +57,33 @@ def acelerar(t, F, estavel):
 def rotacionar(img, angulo):
     comando = pygame.key.get_pressed()
     if comando[pygame.K_a] and pos[1] < 400 or comando[pygame.K_LEFT]:
-        angulo += 5
+        angulo += 1
     if comando[pygame.K_z] and pos[1] < 400 or comando[pygame.K_RIGHT]:
-        angulo -= 5
-    #Limitar o angulo em 35°
-    if angulo > 60:
-        angulo = 60
-    if angulo < -60:
-        angulo = -60
+        angulo -= 1
+    # Limitar o angulo em 35°
+    if angulo > 35:
+        angulo = 35
+    if angulo < -35:
+        angulo = -35
     drone_rodado = pygame.transform.rotate(img, angulo)
-    posr = (pos[0] - drone_rodado.get_width()/10, pos[1] - drone_rodado.get_height()/4)
+    posr = (pos[0] - drone_rodado.get_width()/10,
+            pos[1] - drone_rodado.get_height()/4)
     return drone_rodado
 
-def auto_estabilizar(estavel):
-    comando = pygame.key.get_pressed()
-    if comando[pygame.K_g]:
-        estavel = True
-    if comando[pygame.K_e]:
-        estavel = False
-    
-    return estavel
+
+def auto_estabilizar(angulo):
+    if angulo > 0:
+        angulo -= 1
+    else:
+        angulo += 1
+
+    return angulo
+
 
 def mouse1(pos, angulo, eP, ePhi, mx, my, x, t):
-    if np.abs(eP[0])> 0.2 or np.abs(eP[1])> 0.2 or np.abs(ePhi)> 0.2:
+    if np.abs(eP[0]) > 0.2 or np.abs(eP[1]) > 0.2 or np.abs(ePhi) > 0.2:
         x, eP, ePhi = executePID(x, [mx, my], t)
-        #print(x[2:4])
+        # print(x[2:4])
         #print([mx, my])
 
         posH = x[2]
@@ -91,8 +94,9 @@ def mouse1(pos, angulo, eP, ePhi, mx, my, x, t):
         return pos, ang
     return pos, ang
 
+
 def mouse2(pos, angulo, eP, ePhi, mx, my, x, t):
-    if np.abs(eP[0]) > 0.2 or np.abs(eP[0])> 0.2 or np.abs(eP[0])> 0.2:
+    if np.abs(eP[0]) > 0.2 or np.abs(eP[0]) > 0.2 or np.abs(eP[0]) > 0.2:
         x, eP, ePhi = executePID(x, [mx, my], t)
         posH = x[2]
         posV = x[3]
@@ -101,16 +105,17 @@ def mouse2(pos, angulo, eP, ePhi, mx, my, x, t):
         pos = [posH, posV]
         #print(mx, my)
         return True
-    return False  
+    return False
+
 
 def velocidade_resultante(pos, F, P, m, estavel, angulo, step, vel_vert, vel_horiz, t, y_direction):
     Fx = Fy = -1
-    #Calculo de Força
+    # Calculo de Força
     Fx = F * math.sin(angulo * math.pi/180)
-    #print(Fx)
+    # print(Fx)
     if not estavel:
-        Fy = F * math.cos(angulo * math.pi/180) 
-        #print(Fy)
+        Fy = F * math.cos(angulo * math.pi/180)
+        # print(Fy)
         step -= 0.05
         if step <= 0:
             step = 0
@@ -145,14 +150,15 @@ def velocidade_resultante(pos, F, P, m, estavel, angulo, step, vel_vert, vel_hor
 
     if pos[0] <= 0 or pos[0] >= larg_bk-90:
         pos[0] = pos[0]
-   
+
     if pos[1] <= 0 or pos[1] >= altura_bk-100:
         pos[1] = pos[1]
-    
-    #print(pos)
+
+    # print(pos)
     return pos
 
-def waypoints(pos, F, x, dt, angulo, y_direction):
+
+def waypoints(pos, F, x, dt, angulo, y_direction, i):
     xpos = [0, 0]
     wp = [[0., 10], [15., 25], [-30, 70]]
     pi = [500, 400]
@@ -161,15 +167,15 @@ def waypoints(pos, F, x, dt, angulo, y_direction):
         y_direction = -1
     else:
         y_direction = 0
-    #for i in range(len(wp)):
+    # for i in range(len(wp)):
     xpos[0] = pi[0] + wp[0][0]
     xpos[1] = pi[1] + wp[0][1]
     print(xpos)
     xpos, angulo = mouse1(pos, angulo, eP, ePhi, wp[0][0], wp[0][1], x, dt)
     # xpos = velocidade_resultante(pos, F, P, m, estavel, angulo, step, vel_vert, vel_horiz, dt, y_direction)
-    return xpos, angulo 
+    return xpos, angulo
 
-        
+
 # Inicia o pygame
 pygame.init()
 # Carrega as imagens e define medidas
@@ -208,8 +214,8 @@ font_padrao = pygame.font.get_default_font()
 font = pygame.font.SysFont(font_padrao, 30)
 
 while running:
-    clock.tick(60)
-    #print(t)
+    #clock.tick(60)
+    # print(t)
     clock.tick(fps)  # Define fps
 
     tela.fill([0, 0, 0])  # Cria tela
@@ -225,80 +231,104 @@ while running:
             sys.exit()
 # Eventos de controle de drone
     # Estado das variáveis
-    x = np.array([w1, w2, pos[0], pos[1], v1, v2, angulo * np.pi/180., vel_ang*np.pi/180.])
+    x = np.array([w1, w2, pos[0], pos[1], v1, v2,
+                 angulo * np.pi/180., vel_ang*np.pi/180.])
 
     comando = pygame.key.get_pressed()
-    #Inércia do sistema
+    # Inércia do sistema
     if not comando[pygame.K_DOWN] and not comando[pygame.K_UP]:
         y_direction = 0
-    if comando[pygame.K_LEFT]:#Comando para esquerda
+        grav = True
+    if comando[pygame.K_LEFT]:  # Comando para esquerda
         pos[0] -= step
         if pos[0] <= 0:
             pos[0] += step
         grav = False
         auto_move = False
 
-    if comando[pygame.K_RIGHT]:#Comando para direita
+    if comando[pygame.K_RIGHT]:  # Comando para direita
         right = True
         pos[0] += step
         if pos[0] <= 0 or pos[0] >= larg_bk-90:
             pos[0] -= step
         grav = False
+        auto_move = False
 
-    if comando[pygame.K_UP]:#Comando para cima
+    if comando[pygame.K_UP]:  # Comando para cima
         pos[1] -= step
-        if pos[1] <= 0 or pos[1] >= altura_bk:#Colisão
+        if pos[1] <= 0 or pos[1] >= altura_bk:  # Colisão
             pos[1] += step
         grav = False
+        auto_move = False
 
-    if comando[pygame.K_DOWN]:#Comando para cima
+    if comando[pygame.K_DOWN]:  # Comando para cima
         pos[1] += step
         if pos[1] > 400:
             angulo = angulo
             grav = False
             pos[1] = 400
-    #Comando para seguir caminho dos waypoints
+        auto_move = False
+    # Comando para seguir caminho dos waypoints
     if comando[pygame.K_w]:
         pygame.time.delay(100)
-        pos, angulo = waypoints(pos, F, x, dt, angulo, y_direction)
-    #Eventos de clique do mouse
+        i = 0
+        pos, angulo = waypoints(pos, F, x, dt, angulo, y_direction, i)
+    # Eventos de clique do mouse
     if event.type == pygame.MOUSEBUTTONDOWN:
         auto_move = True
         mx, my = pygame.mouse.get_pos()
-    #Posicionamento do drone nos limites da tela
-    if pos[1] >= my or pos[1]> 350:
-        grav = False
+    # Posicionamento do drone nos limites da tela
+    if pos[1] >= my or pos[1] > 450:
+        #grav = False
         angulo = angulo
         pos[1] = pos[1]
 
     if angulo > 360:
         angulo = 0
 
-    ## Movimento automatico definido pelo clique do mouse
+    # Movimento automatico definido pelo clique do mouse
     if auto_move:
-        if np.abs(my - pos[1]) > 5:
+        estavel = True
+        if (my - pos[1]) < 5:
             F = acelerar(dt, F, estavel)
             y_direction = -1
         else:
             y_direction = 0
+        angulo = auto_estabilizar(angulo)
         pos, angulo = mouse1(pos, angulo, eP, ePhi, mx, my, x, dt)
 
     else:
-        auto_move = False
-    # xd = 0.1*pos[0] - 40
-    # yd = -(pos[1]-500)/275
+        grav = True
+
+    xd = 0.1*pos[0] - 40
+    yd = -(pos[1]-500)/275
     # dpos = [400, 500]
     #print(xd, yd, '=' , pos[0], pos[1])
+    if pos[1] < 600:
+        grav = False
     if grav is True:
         pos[1] += step
     else:
         pos[1] = pos[1]
-    #Funções de movimentação do drone na tela
-    drone_rodado = rotacionar(img_drone, angulo)# Atua na estabilização da rotação do drone em movimento
-    pos = velocidade_resultante(pos, F, P, m, estavel, angulo, step, vel_vert, vel_horiz, dt, y_direction) #Movimenta o drone 
+    # Funções de movimentação do drone na tela
+    # Atua na estabilização da rotação do drone em movimento
+    drone_rodado = rotacionar(img_drone, angulo)
+    pos = velocidade_resultante(pos, F, P, m, estavel, angulo, step,
+                                vel_vert, vel_horiz, dt, y_direction)  # Movimenta o drone
     tela.blit(drone_rodado, pos)  # Atualiza o drone na tela
 
-    wp = [[0., 10], [15., 25], [-30, 70]] #waypoints
+    wp = [[0., 10], [15., 25], [-30, 70]]  # waypoints
+    print(grav)
+    # if np.abs(yd -wp[0][1])< 0.000001:
+    #     print("Perto")
+    # tp = np.array([w1, w2, xd, yd, v1, v2, angulo * np.pi/180., vel_ang*np.pi/180.])
+
+    # tp , eTP, eTPhi = executePID(tp, wp[0], dt)
+    # print(yd)
+    # if np.abs(tp[3] - wp[0][1]) > 8.3:
+    #     pos[1] -=1
+    # else:
+    #     pos[1] = pos[1]
 
     pygame.time.delay(10)
 
